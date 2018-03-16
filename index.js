5fbad722251c089ea95352405d125723b42dffa8
Yuval Dagai <Yuval.dagai@ca.com>
2018-03-16 14:14:46 -0700

    add log message

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const express = require("express");
 
 const app = express();
 directline.initializeRoutes(app, "http://127.0.0.1:3000", "http://127.0.0.1:3978/api/messages");
+
+console.log('Routing http://localhost:3000/directline -> http://127.0.0.1:3978/api/messages');
